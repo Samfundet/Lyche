ee3598a7cc843614db4e0d072ecddfac945228b7
Jonas Myrlund <myrlund@gmail.com>
2012-02-08 18:31:33 +0100

    Initial commit. Vanilla Rails 3.1 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lyche::Application.config.secret_token = 'befc1a82f70e06261f751d32589ed8503e49ef2093d615bd7a115deb1ba08ed677fe7ada252fd88d2a5cb3da01d7ebe73a7449c4350717e38156143f255f9066'
