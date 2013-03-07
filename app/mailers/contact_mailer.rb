60f965f615eafdc19d0ff9ddc23488963f34dcf2
Anders Wold Eldhuset <andereld@samfundet.no>
2013-03-08 00:32:27 +0100

    Change 'To' address from mine to Lyche's on /contact

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ActionMailer::Base
   default from: "no-reply@samfundet.no"
-  default to: "mg-websjef@samfundet.no"
+  default to: "lyche@samfundet.no"
 
   def new_message(message)
       @message = message
