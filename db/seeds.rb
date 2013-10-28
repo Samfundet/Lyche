76bc04938ee39cd24726f50b800daf79e50b5730
Anders Wold Eldhuset <andereld@samfundet.no>
2013-10-28 23:58:06 +0100

    Update Lyche's menu
    This is in response to RT #58424.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,39 @@ Dish.delete_all
 
 # Create categories
 n = 1
-["Barsnacks", "Lunchretter", "Hovedretter", "Desserter"].each do |category_title|
+["Barsnacks", "Småretter", "Hovedretter", "Desserter", "For små Lychetroll"].each do |category_title|
   category = Category.create(title: category_title, position: n)
   n += 1
 end
 
 # Barsnacks
 barsnacks = Category.find_by_title("Barsnacks")
-barsnacks.dishes.create(title: "Focacciasnack", description: "Hjemmelaget focacciabrød, serveres med pistou og aioli. Vegetar.", member_price: 19, regular_price: 29)
-barsnacks.dishes.create(title: "Varm mandelpotet- og chorizosnack", description: "Ovnsbakte mandelpoteter med chorizo og gressløk.", member_price: 35, regular_price: 39)
+barsnacks.dishes.create(title: "Focacciasnack", description: "Lyches hjemmelagede focaccia, serveres med aioli.", member_price: 19, regular_price: 29)
+barsnacks.dishes.create(title: "Chorizosnack", description: "Chorizobakte småpoteter med pistou og en skål røkt BBQ­saus.", member_price: 39, regular_price: 44)
+barsnacks.dishes.create(title: "Sidesalat", description: "En liten salat. Kan nytes for seg selv, eller til andre retter.", member_price: 29, regular_price: 39)
 
-lunch = Category.find_by_title("Lunchretter")
-lunch.dishes.create(title: "Sesongsuppen", description: "Suppe etter sesongens råvarer. Spør i baren. Ofte vegetar.", member_price: 55, regular_price: 65)
-lunch.dishes.create(title: "Grillet Lychebrød", description: "1. Grillet focacciabrød med and og spansk geitost. Toppes med frisk salat. Kan fås som vegetar.\n2. Grillet focacciabrød med chorizo og spansk geitost. Toppes med frisk salat.", member_price: 69, regular_price: 79)
-lunch.dishes.create(title: "Lun Lychesalat", description: "Med and, bacon, og krydret kikertsaus. Serveres med hjemmelaget focacciabrød og tzatsiki. Kan fås som vegetar.", member_price: 69, regular_price: 79)
+lunch = Category.find_by_title("Småretter")
+lunch.dishes.create(title: "Suppeliten", description: "Månedens suppe, servert i forrettstørrelse. Kan ofte fås som vegetar, spør i baren.", member_price: 39, regular_price: 44)
+lunch.dishes.create(title: "Hvitvinsdampede blåskjell", description: "Dampede blåskjell i syrlig, kremet saus.", member_price: 89, regular_price: 114)
+lunch.dishes.create(title: "Chorizopasta", description: "Pasta med kremet, pikant chorizosaus og manchegodryss.", member_price: 69, regular_price: 84)
 
 main = Category.find_by_title("Hovedretter")
-main.dishes.create(title: "Lycheburger", description: "Lyches ubestridte klassiker. Med aioli, pistou, bacon, cheddarost og paprikasalsa. Serveres med ovnsbakte mandelpoteter.", member_price: 99, regular_price: 109)
-main.dishes.create(title: "Dobbel Lycheburger", description: "Lycheburger i dobbeltdekkervariant. 260 gram ren Lyche.", member_price: 139, regular_price: 149)
-main.dishes.create(title: "Vegetarburger", description: "Lyches vegetarburger. Med aioli, pistou, cheddarost, salat og paprikasalsa. Serveres med ovnsbakte mandelpoteter.", member_price: 89, regular_price: 99)
-main.dishes.create(title: "Confiterte andelår", description: "Langtidsstekt, sprøtt andelår. Serveres med ovnsbakte grønnsaker, pastinakkpuré, appelsinsaus og ovnsbakte mandelpoteter.", member_price: 119, regular_price: 129)
-main.dishes.create(title: "Ovnsbakt lakseloin", description: "Lakseloin med ovnsbakte grønnsaker og mandelpoteter, samt pastinakkpuré. Toppes med mandelvinaigrette.", member_price: 119, regular_price: 129)
-main.dishes.create(title: "Greske lammepølser", description: "Lyches hjemmelagede lammepølser med karibisk mojosaus, tzatsiki og chorizobakte poteter.", member_price: 99, regular_price: 109)
-main.dishes.create(title: "Grillet iberisk svin", description: "Frittgående iberisk svin med nøttepreg. Serveres med ovnsbakte grønnsaker, pastinakkpuré, rødvinsreduksjon og ovnsbakte mandelpoteter.", member_price: 145, regular_price: 159)
-main.dishes.create(title: "Grillplanke", description: "Vår grillplanke med lammepølse, iberisk svin og andelår. Serveres med chorizobakte poteter, karibisk mojosaus og tzatsiki.", member_price: 145, regular_price: 159)
+main.dishes.create(title: "Lycheburger", description: "Lyches ubestridte klassiker. Serveres med aioli, bacon, cheddarost, pistou, paprikasalsa og ovnsbakte småpoteter. Burgeren inneholder svin.", member_price: 99, regular_price: 119)
+main.dishes.create(title: "Dobbel Lycheburger", description: "Lycheburger i dobbeltdekkervariant. 250 gram ren Lyche. Serveres med aioli, bacon, cheddarost, pistou, paprikasalsa og ovnsbakte småpoteter. Burgeren inneholder svin.", member_price: 139, regular_price: 155)
+main.dishes.create(title: "Kykkeliburger", description: "Lyches kyllingburger. Serveres med aioli, manchegodressing, pistou og ovnsbakte småpoteter.", member_price: 99, regular_price: 119)
+main.dishes.create(title: "Vegetarburger", description: "Lyches vegetarburger. Serveres med aioli, cheddarost, pistou, paprikasalsa og ovnsbakte småpoteter.", member_price: 89, regular_price: 109)
+main.dishes.create(title: "Gladlaks", description: "Ovnsbakt lakseloin, serveres med mandelvinaigrette, søtpotetpuré, småpoteter og ovnsbakte grønnsaker.", member_price: 119, regular_price: 139)
+main.dishes.create(title: "Pluma", description: "Frittgående iberisk svin fra Spania. Servers med søtpotetpuré, syrlig rødvinsreduksjon, småpoteter og ovnsbakte grønnsaker.", member_price: 149, regular_price: 169)
+main.dishes.create(title: "Heldiggrisen Babe", description: "BBQ­glasert skinkeskive med smørdampet pak choi, søtpotetpuré og ­timbal, småpoteter og dijonkrem.", member_price: 99, regular_price: 119)
+main.dishes.create(title: "Heisann Portobello", description: "Ovnsbakt portobellosopp. Fylt med tomat­ og quinoasaus, og gratinert med manchego. Serveres med søtpotetpuré og –timbal, og soltørket tomat. Vegetar.", member_price: 99, regular_price: 119)
+main.dishes.create(title: "Frisk falafelfryd", description: "Salat med falafler, sprøstekt bacon, marinerte kikerter og sukkererter. Serveres med focaccia og aioli.", member_price: 79, regular_price: 99)
+main.dishes.create(title: "Månedens suppe", description: "Hver måned disker Lychekjøkkenet opp med en ny suppe. Serveres med focaccia og smør. Kan ofte fås som vegetar, spør i baren.", member_price: 59, regular_price: 75)
 
 desserts = Category.find_by_title("Desserter")
-desserts.dishes.create(title: "Sjokoladelyche", description: "Konfektkake av fyldig sjokolade, med pisket krem og bærsaus.", member_price: 35, regular_price: 45)
-desserts.dishes.create(title: "Panna cotta", description: "Panna cotta med bærsaus.", member_price: 30, regular_price: 35)
+desserts.dishes.create(title: "Karl Oris favoritt", description: "En mektig sjokoladeterrin fylt med Oreokjeks. Toppes med syrlig ostekrem og bringebærcoulis.", member_price: 49, regular_price: 59)
+desserts.dishes.create(title: "Flambert lidenskap", description: "Créme brûlée med pasjonsfrukt. Serveres med bringebærsorbét.", member_price: 39, regular_price: 49)
+desserts.dishes.create(title: "Tilslørte bondepiker", description: "En dessert av crumble, eplemos og syrlig ostekrem. Servers med bringebærcoulis.", member_price: 39, regular_price: 49)
+
+childrens_dishes = Category.find_by_title("For små Lychetroll")
+childrens_dishes.dishes.create(title: "Lycheliten", description: "En mindre variant av Lycheburgeren.", member_price: 75, regular_price: 75)
+childrens_dishes.dishes.create(title: "Nasse Nøff", description: "En liten variant av glasert skinke.", member_price: 75, regular_price: 75)
